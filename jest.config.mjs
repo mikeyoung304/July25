284f37e3bfb9fa1c4d1920470a1e31ad5ba41e6c
mikeyoung304 <mikeyoung304@gmail.com>
2025-07-15 08:54:57 -0400

    test: Jest ESM working – suites green

---
+++ jest.config.mjs
@@ -0,0 +1,49 @@
+export default {
+  projects: [
+    {
+      displayName: 'client',
+      testEnvironment: 'jsdom',
+      rootDir: 'client',
+      testMatch: ['<rootDir>/src/**/*.{test,spec}.{ts,tsx,js,jsx}'],
+      setupFiles: ['<rootDir>/src/test/jest-globals.js'],
+      setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+      transform: {
+        '^.+\\.(ts|tsx)$': ['ts-jest', {
+          tsconfig: {
+            jsx: 'react-jsx',
+            esModuleInterop: true,
+            allowSyntheticDefaultImports: true,
+            target: 'ES2020',
+            module: 'ESNext',
+          },
+        }],
+      },
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+      },
+      extensionsToTreatAsEsm: ['.ts', '.tsx'],
+      globals: {
+        'ts-jest': {
+          useESM: true,
+        },
+      },
+    },
+    {
+      displayName: 'server',
+      testEnvironment: 'node',
+      rootDir: 'server',
+      testMatch: ['<rootDir>/**/*.{test,spec}.{ts,js}'],
+      transform: {
+        '^.+\\.ts$': ['ts-jest', {
+          tsconfig: {
+            esModuleInterop: true,
+            allowSyntheticDefaultImports: true,
+            target: 'ES2020',
+            module: 'commonjs',
+          },
+        }],
+      },
+    },
+  ],
+};
